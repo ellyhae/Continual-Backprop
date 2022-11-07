58aca86c9a2112e09f58311c47e772134dfbf196
chripp <31983212+chripp@users.noreply.github.com>
2022-11-07 21:26:00 +0100

    added per-process tensorboard patching
    Set wandb tensorboard dir once per process. Also close VideoRecorder env before wandb.finish() to avoid errors in the destructor

--- hydra_launcher.py
+++ hydra_launcher.py
@@ -10,6 +10,7 @@ from wandb.integration.sb3 import WandbCallback
 
 from omegaconf import DictConfig, OmegaConf
 import hydra
+from hydra.core.hydra_config import HydraConfig
 
 from stable_baselines3.ppo import PPO
 from stable_baselines3.common.monitor import Monitor
@@ -27,14 +28,20 @@ def run_experiment(cfg: DictConfig) -> None:
     np.random.seed(cfg.random.seed)
     torch.manual_seed(cfg.random.seed)
     
+    hydra_cfg = HydraConfig.get()
+    if hydra_cfg.job.num < hydra_cfg.launcher.n_jobs:
+        wandb.tensorboard.patch(root_logdir=cfg.wandb.tensorboard_root_dir)
+    
     run = wandb.init(
         project="CPPO",
         group=cfg.wandb.group,
         name=cfg.name,
-        config=OmegaConf.to_container(cfg),
+        config=OmegaConf.to_container(cfg, resolve=True),
+        dir=cfg.wandb.dir,
         sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
         monitor_gym=True,  # auto-upload the videos of agents playing the game
-        mode=cfg.wandb.mode
+        mode=cfg.wandb.mode,
+        reinit=True
     )
     
     env = Monitor(SlidingAntEnv(cfg.total_timesteps//cfg.n_jumps, max_steps=cfg.train_max_steps))
@@ -43,7 +50,7 @@ def run_experiment(cfg: DictConfig) -> None:
     
     learner_class = "MlpPolicy" if cfg.algorithm.policy == "MlpPolicy" else CPPO_Policy
     
-    settings = OmegaConf.to_container(cfg.algorithm.settings)
+    settings = OmegaConf.to_container(cfg.algorithm.settings, resolve=True)
     settings['policy_kwargs']['activation_fn'] = getattr(torch.nn, settings['policy_kwargs']['activation_fn'])
     
     ppo = PPO(learner_class, env, seed=int(cfg.random.seed), **settings)
@@ -52,8 +59,8 @@ def run_experiment(cfg: DictConfig) -> None:
     ppo.learn(total_timesteps=cfg.total_timesteps, callback=callbacks, tb_log_name=cfg.name)
     
     ppo.save(os.path.join(cfg.model_dir, cfg.name))
-    del env  # wandb has logging in the environment destructor, which needs to be called before run.finish(). Since del cannot be forced, we also do not use run.finish and just pray
-    #run.finish()
+    env.close()  # wandb has logging in env.close(), which is called in the destructor of env. Call explicitly before run.finish(), otherwise wandb connection could be closed before logging (late destructor call)
+    run.finish()
 
 if __name__ == "__main__":
     run_experiment()
