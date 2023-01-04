dd089f966d40bd571ebe7bb196101413c9e4391c
chripp <31983212+chripp@users.noreply.github.com>
2023-01-04 11:59:37 +0100

    removed joblib backend
    spawning vec envs inside joblib causes errors, so it will not be used. Instad, if necessary, multiple hydra sweeps can be started manually

--- hydra_launcher.py
+++ hydra_launcher.py
@@ -24,8 +24,6 @@ from env import SlidingAntEnv
 from utils import WeightLogger, AgesLogger, SlidingEval
 from sb3_logger import configure_logger, WandbOutputFormat
 
-wandb.setup()
-
 @hydra.main(version_base=None, config_path="hydra_config", config_name="config")
 def run_experiment(cfg: DictConfig) -> None:
     
@@ -72,4 +70,5 @@ def run_experiment(cfg: DictConfig) -> None:
     ppo.logger.close()
 
 if __name__ == "__main__":
+    wandb.setup()
     run_experiment()
