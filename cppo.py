d121428b8c5163f259db06af60208804300c50bb
chripp <31983212+chripp@users.noreply.github.com>
2023-01-08 15:08:20 +0100

    fixed error when no optimizer kwargs given

--- cppo.py
+++ cppo.py
@@ -10,7 +10,7 @@ class CPPO_Policy(ActorCriticPolicy):
         **kwargs
     ):
         super(CPPO_Policy, self).__init__(observation_space, action_space, lr_schedule, **(kwargs|{'optimizer_kwargs':{}})) # remove optimizer_kwargs, as __init__ initializes Adam with them, which throws errors
-        self.optimizer_kwargs = kwargs['optimizer_kwargs']
+        self.optimizer_kwargs = kwargs.get('optimizer_kwargs', {})
         assert len(self.mlp_extractor.shared_net) == 0, 'no shared layers between policy and value function allowed' # not used in the paper, might try to implement it later
         
         policy_linears, policy_activations = self._handle_sequential(self.mlp_extractor.policy_net)
